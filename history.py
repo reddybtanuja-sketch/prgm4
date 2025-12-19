677f30e58563f117fd30fee4048e64b944decb4f
reddybtanuja-sketch <reddybtanuja@gmail.com>
2025-12-19 13:32:46 +0530

    Add files via upload

---
+++ history.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Thu Dec 26 11:37:19 2024)---
+runfile('C:/Users/admin/untitled0.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/untitled1.py', wdir='C:/Users/admin')
+
+## ---(Thu Dec 26 14:29:28 2024)---
+runfile('C:/Users/admin/untitled0.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/untitled0.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/untitled0.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/untitled0.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/untitled0.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/untitled0.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/untitled0.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/untitled1.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/untitled0.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/untitled0.py', wdir='C:/Users/admin')
+
+## ---(Sat Jan  4 14:11:04 2025)---
+runfile('C:/Users/admin/pg3.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/p3.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/pg3.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/p3.py', wdir='C:/Users/admin')
+
+## ---(Sun Feb  9 14:16:02 2025)---
+runfile('C:/Users/admin/prg.py', wdir='C:/Users/admin')
+
+## ---(Fri Feb 14 09:52:54 2025)---
+runfile('C:/Users/admin/pg3.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/p3.py', wdir='C:/Users/admin')
+
+## ---(Tue Apr  1 10:18:45 2025)---
+runfile('C:/Users/admin/prg6.py', wdir='C:/Users/admin')
+
+## ---(Tue Apr  8 09:47:04 2025)---
+runfile('C:/Users/admin/prg7.py', wdir='C:/Users/admin')
+
+## ---(Tue Jun 10 09:00:30 2025)---
+runfile('C:/Users/admin/d.py', wdir='C:/Users/admin')
+
+## ---(Tue Jun 10 12:07:18 2025)---
+runfile('C:/Users/admin/untitled1.py', wdir='C:/Users/admin')
+
+## ---(Tue Jun 10 14:14:15 2025)---
+runfile('C:/Users/admin/.spyder-py3/pg5.py', wdir='C:/Users/admin/.spyder-py3')
+runfile('C:/Users/admin/.spyder-py3/pg5.py', wdir='C:/Users/admin/.spyder-py3')
+
+## ---(Wed Jun 11 09:08:33 2025)---
+runfile('C:/Users/admin/.spyder-py3/pg5.py', wdir='C:/Users/admin/.spyder-py3')
+runfile('C:/Users/admin/.spyder-py3/untitled0.py', wdir='C:/Users/admin/.spyder-py3')
+debugfile('C:/Users/admin/.spyder-py3/untitled0.py', wdir='C:/Users/admin/.spyder-py3')
+runfile('C:/Users/admin/.spyder-py3/prg2.py', wdir='C:/Users/admin/.spyder-py3')
+runfile('C:/Users/admin/.spyder-py3/pg5.py', wdir='C:/Users/admin/.spyder-py3')
+runfile('C:/Users/admin/.spyder-py3/prg2.py', wdir='C:/Users/admin/.spyder-py3')
+runfile('C:/Users/admin/.spyder-py3/pg5.py', wdir='C:/Users/admin/.spyder-py3')
+runfile('C:/Users/admin/.spyder-py3/pg5.py', wdir='C:/Users/admin/.spyder-py3')
+runfile('C:/Users/admin/.spyder-py3/untitled2.py', wdir='C:/Users/admin/.spyder-py3')
+
+## ---(Wed Jun 11 13:25:09 2025)---
+runfile('C:/Users/admin/untitled1.py', wdir='C:/Users/admin')
+
+## ---(Mon Jun 16 09:13:32 2025)---
+runcell(0, 'C:/Users/admin/untitled0.py')
+runfile('C:/Users/admin/untitled0.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/sre.py', wdir='C:/Users/admin')
+
+## ---(Mon Jun 16 14:49:04 2025)---
+runfile('C:/Users/admin/untitled0.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/untitled1.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/untitled0.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/untitled1.py', wdir='C:/Users/admin')
+
+## ---(Wed Jun 18 09:05:42 2025)---
+runfile('C:/Users/admin/bm35sru.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/bm35sru.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/bm35sru.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/bm35sru.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/bm35sru.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/bm35sru.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/bm35sru.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/untitled0.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/untitled0.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/untitled0.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/untitled0.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/untitled0.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/untitled0.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/bm35sru.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/bm35sru.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/bm35sru.py', wdir='C:/Users/admin')
+
+## ---(Fri Jun 20 11:19:25 2025)---
+runfile('C:/Users/admin/bm35sru.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/sre.py', wdir='C:/Users/admin')
+athish
+runfile('C:/Users/admin/sre.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/untitled2.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/untitled3.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/untitled4.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/untitled5.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/sre.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/untitled5.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/untitled0.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/untitled3.py', wdir='C:/Users/admin')
+runfile('C:/Users/admin/untitled8.py', wdir='C:/Users/admin')
+
+## ---(Fri Jun 27 09:26:42 2025)---
+runfile('C:/Users/admin/untitled2.py', wdir='C:/Users/admin')
+
+## ---(Fri Jun 27 09:48:56 2025)---
+runfile('C:/Users/admin/nandan5th.py', wdir='C:/Users/admin')
+debugfile('C:/Users/admin/nandan5th.py', wdir='C:/Users/admin')
+
+## ---(Fri Jun 27 11:44:53 2025)---
+runfile('C:/Users/admin/pradeep.py', wdir='C:/Users/admin')
+debugfile('C:/Users/admin/pradeep.py', wdir='C:/Users/admin')
+
+## ---(Fri Jun 27 12:12:01 2025)---
+runfile('C:/Users/admin/untitled2.py', wdir='C:/Users/admin')
+
+## ---(Fri Jun 27 14:28:11 2025)---
+runfile('C:/Users/admin/untitled4.py', wdir='C:/Users/admin')
+
+## ---(Fri Jun 27 14:56:39 2025)---
+runcell(0, 'C:/Users/admin/.spyder-py3/temp.py')
+
+## ---(Mon Jun 30 09:55:48 2025)---
+runfile('C:/Users/admin/.spyder-py3/temp.py', wdir='C:/Users/admin/.spyder-py3')
+runfile('C:/Users/admin/.spyder-py3/untitled0.py', wdir='C:/Users/admin/.spyder-py3')
